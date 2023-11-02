ea19a63d0fa38196a8f76e457201647422a9b9dc
iworsham <15iworsham@gmail.com>
2023-11-02 12:30:44 -0400

    changes

--- GalaxyQuest/Controllers/PlanetsController.cs
+++ GalaxyQuest/Controllers/PlanetsController.cs
@@ -1,15 +1,27 @@
 ﻿using GalaxyQuest.Services;
 using Microsoft.AspNetCore.Mvc;
+using GalaxyQuest.Models;
+using GalaxyQuest.Interfaces;
+using GalaxyQuest.Services;
 
 namespace GalaxyQuest.Controllers
 {
     public class PlanetsController : Controller
     {
+        
+        private readonly IPlanetService _planetService;
+
+        public PlanetsController(ILogger<HomeController>logger, IPlanetService planetService)
+        {
+            
+            _planetService = planetService;
+        }
         public IActionResult Index()
         {
             var milkyWayPlanets = MilkyWayGalaxy.Planets;
-            
-            
+          // var planets = await _planetService.GetPlanets();
+
+           
             return View(milkyWayPlanets);
         }
     }
